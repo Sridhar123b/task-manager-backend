c1c1e9dc499162dddac8c765dbe38b3dae966e8c
botta sridhar <bottasridhar159@gmail.com>
2026-05-14 13:30:47 +0530

    Team Task Manager Project

---
+++ backend/src/main/java/com/taskmanager/backend/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.taskmanager.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.taskmanager.backend.model.User;
+import com.taskmanager.backend.repository.UserRepository;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/signup")
+    public User signup(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+    @PostMapping("/login")
+public User login(@RequestBody User loginUser) {
+
+    return userRepository.findAll()
+            .stream()
+            .filter(user ->
+                    user.getEmail().equals(loginUser.getEmail()) &&
+                    user.getPassword().equals(loginUser.getPassword()))
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("Invalid email or password"));
+}
+}
---
+++ backend/src/main/java/com/taskmanager/backend/controller/TaskController.java
@@ -0,0 +1,56 @@
+package com.taskmanager.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.taskmanager.backend.model.Task;
+import com.taskmanager.backend.repository.TaskRepository;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+
+        task.setTitle(updatedTask.getTitle());
+        task.setStatus(updatedTask.getStatus());
+
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteTask(@PathVariable Long id) {
+
+        taskRepository.deleteById(id);
+
+        return "Task deleted successfully";
+    }
+}
